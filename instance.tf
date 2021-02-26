3779b6c0e7a50b7b8af896078c09cb94203dabd7
Rahul meena <rahul159meena@gmail.com>
2021-02-26 19:07:43 +0530

    instance - update

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 
 resource "aws_instance" "db" {
     ami           = "ami-047a51fa27710816e"
-    instance_type = "t2.micro"
+    instance_type = "t2.nano"
   
     tags = {
       "Name" = var.Tagname
